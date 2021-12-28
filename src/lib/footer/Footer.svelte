526674177b4c25582e6d7df455544f97b2d024a3
Orlando J. Vilau <ojvilau@gmail.com>
2021-12-27 23:41:52 -0500

    update footer

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,16 @@
+<div>
+	<footer class="footer">
+		Guidelines | FAQ | Lists | API | Security | Legal | Apply to YC | Contact
+	</footer>
+</div>
+
+<style>
+	.footer {
+		padding: 10px 0 40px;
+		color: #ccc;
+		font-size: 14px;
+		text-align: center;
+		border-top: 1px solid;
+		margin-top: 50px;
+	}
+</style>
